71c1d6e2f09ea0ee452047cbdde1aa78b6e57363
Jon Åby Bergquist <jonbergq@stud.ntnu.no>
2020-01-08 11:20:14 +0100

    Model for ticket added

---
+++ backend/migrations/20200108101418-create-tickets.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ticket_name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      ticket_amount: {
+        type: Sequelize.INTEGER
+      },
+      date_start: {
+        type: Sequelize.DATE
+      },
+      date_end: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tickets');
+  }
+};
