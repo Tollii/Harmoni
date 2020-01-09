474716ebe876b37b651002bc5070ce3e5720326a
Kasper Vedal Gundersen <kaspervg@stud.ntnu.no>
2020-01-09 11:20:30 +0100

    async

--- backend/dao/event_types.js
+++ backend/dao/event_types.js
@@ -1,27 +1,27 @@
 module.exports = models => {
   const Event_types = models.Event_Types;
   return {
-    event_typesAll: () =>
+    event_typesGetAll: async () =>
       Event_types.findAll().then(event_types => event_types),
 
-    event_typesOne: id =>
+    event_typesGetOne: async id =>
       Event_types.findOne({ where: { id: id } }).then(
         event_types => event_types
       ),
 
-    event_typesDelete: id =>
+    event_typesDelete: async id =>
       Event_types.findOne({ where: { id: id } }).then(
         Event_types.destroy({ where: { id: id } }).then(
           event_types => event_types
         )
       ),
 
-    event_typesCreate: event_type =>
+    event_typesCreate: async event_type =>
       Event_types.create({
         event_type: event_type
       }).then(event_type => event_type),
 
-    event_typesUpdate: (id, event_type) => {
+    event_typesUpdate: async (id, event_type) => {
       return Event_types.findOne({ where: { id: id } }).then(() => {
         return Event_types.update(
           { event_type: event_type },
