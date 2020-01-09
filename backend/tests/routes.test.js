2541a2adce2257094288a4f0297cddf498777873
andrtoln <andrtoln@stud.ntnu.no>
2020-01-09 12:23:53 +0100

    removed unneccessary dependencies and imports. Fixed new name for userGetAll in test

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,5 +1,3 @@
-const app = require('../server');
-const request = require('supertest');
 const models = require('../models');
 const userControl = require('../dao/users')(models);
 
@@ -7,7 +5,7 @@ const userControl = require('../dao/users')(models);
 describe('Get Endpoints', () => {
     it('should get all users', async (done) => {
 
-        const res = await userControl.userAll();
+        const res = await userControl.userGetAll();
 
         console.log(res);
         expect(res).toEqual([]);
