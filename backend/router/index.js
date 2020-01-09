949ed6051f8b57b51bf1c2f0d744617bf8d768d3
Kristoffer Vanebo <krisvane@stud.ntnu.no>
2020-01-09 10:44:16 +0100

    fixed conflict

--- backend/router/index.js
+++ backend/router/index.js
@@ -2,9 +2,9 @@ module.exports = (app) => {
 
   const models = require('../models')
 
-  require('./users')(app, models.Users, '/user')
-  require('./event')(app, models.Events, '/event')
-  require('./tickets')(app, models.Tickets, '/ticket')
+  require('./users')(app, models, '/user')
+  require('./event')(app, models, '/event')
+  require('./tickets')(app, models, '/ticket')
 
 
 }
