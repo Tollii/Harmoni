27ecb1c95277687fd4e7394d862534311ecefbf5
Kristoffer Vanebo <krisvane@stud.ntnu.no>
2020-01-09 10:52:17 +0100

    fixed merge conflict

--- backend/router/index.js
+++ backend/router/index.js
@@ -5,6 +5,6 @@ module.exports = (app) => {
   require('./users')(app, models, '/user')
   require('./event')(app, models, '/event')
   require('./tickets')(app, models, '/ticket')
-
+  require('./contracts')(app, models, '/contract')
 
 }
