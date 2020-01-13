516efe0101a6cbf2b12eac456f264f58a2d5cbe5
Mikael Nervik Eidsvaag <mikaelne@stud.ntnu.no>
2020-01-13 12:47:51 +0100

    reverted LoginCard.tsx

--- frontend/src/views/Login/LoginCard.tsx
+++ frontend/src/views/Login/LoginCard.tsx
@@ -8,7 +8,6 @@ import Grid from "@material-ui/core/Grid";
 import Button from "../../components/Button/Button";
 import useForm from "../../service/Form/useForm";
 import validate from "../../service/Form/Validate";
-import login from "../../service/Authentication"
 
 const useStyles = makeStyles({
   grid: {
@@ -31,63 +30,60 @@ const useStyles = makeStyles({
 export default () => {
   const classes = useStyles();
   const { handleChange, handleSubmit, values, errors } = useForm(
-    submit,
-    {
-      email: "",
-      password: ""
-    },
-    validate
+      submit,
+      {
+        email: "",
+        password: ""
+      },
+      validate
   );
 
   function submit() {
     console.log("Submitted form");
     /////////// KODE FOR LOGIN VERIFICATION HER
-    let credentials='{"email": "string", "password": "string"}';
-    console.log("bla bla test")
-    console.log(login.getLogin(values));
   }
 
   return (
-    <Card
-      width={"80%"}
-      style={{ minWidth: "250px", maxWidth: "450px", marginTop: "10%" }}
-    >
-      <Grid container className={classes.grid}>
-        <CardContent>
-          <Grid item>
-            <Typography className={classes.title} variant="h3" align="center">
-              Login
-            </Typography>
-          </Grid>
-
-          <form onSubmit={handleSubmit} noValidate>
-            {errors.email && <Typography>{errors.email}</Typography>}
-
-            <InputField
-              name="email"
-              label="Email"
-              type="text"
-              value={values.email}
-              onChange={handleChange}
-            />
-            {errors.password && <Typography>{errors.password}</Typography>}
-
-            <InputField
-              name="password"
-              label="Password"
-              type="text"
-              autoComplete="current-password"
-              value={values.password}
-              onChange={handleChange}
-            />
-
-            <Grid container direction="row" justify="space-between">
-              <Button>Forgot password?</Button>
-              <Button type="submit" onClick={()=>(submit())}>Log in</Button>
+      <Card
+          width={"80%"}
+          style={{ minWidth: "250px", maxWidth: "450px", marginTop: "10%" }}
+      >
+        <Grid container className={classes.grid}>
+          <CardContent>
+            <Grid item>
+              <Typography className={classes.title} variant="h3" align="center">
+                Login
+              </Typography>
             </Grid>
-          </form>
-        </CardContent>
-      </Grid>
-    </Card>
+
+            <form onSubmit={handleSubmit} noValidate>
+              {errors.email && <Typography>{errors.email}</Typography>}
+
+              <InputField
+                  name="email"
+                  label="Email"
+                  type="text"
+                  value={values.email}
+                  onChange={handleChange}
+              />
+              {errors.password && <Typography>{errors.password}</Typography>}
+
+              <InputField
+                  name="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  value={values.password}
+                  onChange={handleChange}
+              />
+
+              <Grid container direction="row" justify="space-between">
+                <Button>Forgot password?</Button>
+                <Button type="submit">Log in</Button>
+              </Grid>
+            </form>
+          </CardContent>
+        </Grid>
+      </Card>
   );
 };
