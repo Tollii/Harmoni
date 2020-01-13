e1fb5bff27f5356fefb1580ebed32bbd4c61ddb4
Kasper Vedal Gundersen <kaspervg@stud.ntnu.no>
2020-01-13 14:37:32 +0100

    service for riders

---
+++ frontend/src/service/riders.tsx
@@ -0,0 +1,71 @@
+const axios = require("axios").default;
+
+let url = "http://localhost:8080/rider/";
+
+export class Riders {
+  additions!: string;
+  rider_typeID!: number;
+  eventID!: number;
+  token!: string;
+}
+
+class RiderService {
+  getRiders() {
+    return axios.get(url).then((response: { data: JSON }) => response.data);
+  }
+  getRider(rider_type_id: number, event_id: number, token: string) {
+    return axios
+      .get(
+        url +
+          "rider_type/" +
+          rider_type_id +
+          "/event/" +
+          event_id +
+          "/user/" +
+          token
+      )
+      .then((response: { data: JSON }) => response.data);
+  }
+
+  postRider(rider: object) {
+    return axios
+      .post(url, rider)
+      .then((response: { data: JSON }) => console.log(response));
+  }
+
+  updateRider(
+    rider_type_id: number,
+    event_id: number,
+    token: string,
+    rider: object
+  ) {
+    return axios
+      .put(
+        url +
+          "rider_type/" +
+          rider_type_id +
+          "/event/" +
+          event_id +
+          "/user/" +
+          token,
+        rider
+      )
+      .then((response: { data: JSON }) => console.log(response));
+  }
+
+  deleteRider(rider_type_id: number, event_id: number, token: string) {
+    axios
+      .delete(
+        url +
+          "rider_type/" +
+          rider_type_id +
+          "/event/" +
+          event_id +
+          "/user/" +
+          token
+      )
+      .then((response: { data: JSON }) => console.log(response));
+  }
+}
+
+export let riderService = new RiderService();
