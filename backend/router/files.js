e3bc8f9a6a9c5cdabccee7a2f8f1bcc61657b6f3
Kristoffer Vanebo <krisvane@stud.ntnu.no>
2020-01-14 15:02:28 +0100

    added post method

--- backend/router/files.js
+++ backend/router/files.js
@@ -1,4 +1,5 @@
-var express = require('express');
+profilePicturesFolderconst express = require('express');
+const fileUpload = require('express-fileupload');
 
 /**
  * @typedef Profile_picture
@@ -14,9 +15,13 @@ var express = require('express');
  */
 
 module.exports = (app, models, auth) => {
+
+  app.use(express.static(__basedir));
+  app.use(fileUpload());
+
   const userControl = require('../dao/users')(models);
-  const files_path = 'saved_files';
-  const profile_pictures_folder = '/profile_pictures/';
+  const filesPath = __basedir + '/saved_files';
+  const profilePicturesFolder = filespath + '/profile_pictures/';
 
   /**
    * @group Files - operations about files
@@ -28,16 +33,35 @@ module.exports = (app, models, auth) => {
    app.get('/profile_picture/:token', async (req, res) => {
      let id = await auth.decode_token(req.params.token);
      let profile_picture = await userControl.userGetOne(id).then(data => data.picture);
-     res.sendFile(profile_pictures_folder + profile_picture, { root: files_path });
+     res.sendFile(profilePicturesFolder + profile_picture);
    });
 
    /**
     * @group Files - operations about files
     * @route POST /profile_picture/{token}/
-    * @returns {object} 200 - returns profile_picture
+    * @param {string} token.path.required - token
+    * @returns {object} 200 - ok
     * @returns {error} default - unexpected error
     */
    app.post('profile_picture/:token', async (req, res) => {
+     if(!req.files || Object.keys(req.files).length === 0) {
+       res.status(400).send('No files uploaded');
+     } else {
+       let profilePicture = req.files.profile_picture;
+       let id = await auth.decode_token(req.params.token);
+       let user = await userControl.userGetOne(id);
 
+       profilePicture.name = id + '_' + user.username;
+
+       userControl.userUpdate(id, user.username, user.email, user.phone, profilePicture.name);
+
+       profilePicture.mv(profilePicturesFolder + profilePicture.name, function(err) {
+         if(err) {
+           return res.sendStatus(500).send(err);
+         }
+
+         res.send('File uploaded');
+       });
+     }
    });
 }
