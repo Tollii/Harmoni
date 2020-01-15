2b2a37109311578540bf0001e6d883b44b2d3474
Shahed Saeed Abbas <shahedsa@stud.ntnu.no>
2020-01-15 09:21:58 +0100

    added eventCard

---
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import {
+  fade,
+  Theme,
+  createStyles,
+  Link,
+  Button,
+  Grid,
+  Typography
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      maxWidth: "400px",
+      backgroundColor: "#E5E5E5"
+    },
+    image: {
+      transform: "scale(1)",
+      "&:hover": {
+        transform: "scale(1.1)",
+        backgroundColor: fade(theme.palette.common.white, 0.15)
+      }
+    },
+    content: {
+      fontSize: "20px",
+      color: "black",
+      alignItems: "rtl",
+      backgroundColor: "white"
+    },
+    link: {
+      fontSize: "20px"
+    },
+    title: {
+      color: "black",
+      textAlign: "center"
+    }
+  })
+);
+
+export default function EventCard(props: any) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.image}
+          component="img"
+          height="160"
+          image={require("../../assets/img/festival.jpeg")}
+          title="festival"
+        />
+
+        <CardContent>
+          <Grid container spacing={6}></Grid>
+
+          <Grid item>
+            <Button className={classes.content}>Jan 20</Button>
+          </Grid>
+          <Grid item>
+            <Typography className={classes.title} onChange={props.handleChange}>
+              This is Title of the event
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Link color="primary" className={classes.link}>
+              More Information
+            </Link>
+          </Grid>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- frontend/src/views/Main/Main.tsx
+++ frontend/src/views/Main/Main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+//import EventCard from "../../components/EventCard/EventCard";
 
 export default () => {
   return (
