9ee6339ae842c37ea6aa3c5987ac93eaf66d1955
Kristoffer Vanebo <krisvane@stud.ntnu.no>
2020-01-15 08:47:36 +0100

    fixed file extensions and added event imaga get

--- backend/router/files.js
+++ backend/router/files.js
@@ -20,9 +20,13 @@ module.exports = (app, models, auth) => {
   app.use(fileUpload());
 
   const userControl = require('../dao/users')(models);
+  const eventControl = require('../dao/events')(models);
+
   const filesPath = __basedir + '/saved_files';
   const profilePicturesFolder = filesPath + '/profile_pictures/';
+  const eventImagesFolder = filesPath + '/event_images/';
 
+  //// PROFILE PICTURES ////
   /**
    * @group Files - operations about files
    * @route GET /profile_picture/{token}/
@@ -32,8 +36,8 @@ module.exports = (app, models, auth) => {
    */
    app.get('/profile_picture/:token', async (req, res) => {
      let id = await auth.decode_token(req.params.token);
-     let profile_picture = await userControl.userGetOne(id).then(data => data.picture);
-     res.sendFile(profilePicturesFolder + profile_picture);
+     let profilePicture = await userControl.userGetOne(id).then(data => data.picture);
+     res.sendFile(profilePicturesFolder + profilePicture);
    });
 
    /**
@@ -52,7 +56,8 @@ module.exports = (app, models, auth) => {
        let id = await auth.decode_token(req.params.token);
        let user = await userControl.userGetOne(id);
 
-       profilePicture.name = id + '_' + user.username;
+       let splitName = profilePicture.name.split('.');
+       profilePicture.name = id + '_' + user.username + '.' + splitName[splitName.length - 1];
 
        userControl.userUpdate(id, user.username, user.email, user.phone, profilePicture.name);
 
@@ -65,4 +70,26 @@ module.exports = (app, models, auth) => {
        });
      }
    });
+
+   //// EVENT IMAGES ////
+   /**
+    * @group Files - operations about files
+    * @route GET /event_image/{id}/
+    * @param {integer} id.path.required - event id
+    * @param {string} token.query.required - token
+    * @returns {object} 200 - ok
+    * @returns {error} default - unexpected error
+    */
+  app.post('event_image/:id', async (req, res) => {
+    auth.check_permissions(req.query.token, ["Admin", "Organizer"])
+    .then(async data => {
+      if (data.auth) {
+        let eventImage = await eventControl.userGetOne(id).then(data => data.event_image);
+        res.sendFile(eventImagesFolder + eventImage);
+      } else {
+        res.status(400).send("Not authenticated");
+      }
+    })
+    .catch(err => console.log(err));
+  });
 }
