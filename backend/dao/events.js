f556624cc7c45403ae8d30494e50973da4af0ed8
Kristoffer Vanebo <krisvane@stud.ntnu.no>
2020-01-15 08:59:08 +0100

    updated event dao and model with event image

--- backend/dao/events.js
+++ backend/dao/events.js
@@ -40,6 +40,7 @@ module.exports = models => {
       event_start,
       event_end,
       personnel,
+      event_image,
       description,
       archived,
       event_typeID
@@ -56,6 +57,7 @@ module.exports = models => {
             event_start: event_start,
             event_end: event_end,
             personnel: personnel,
+            event_image: event_image,
             description: description,
             archived: archived,
             event_typeID: event_typeID
@@ -95,6 +97,7 @@ module.exports = models => {
       event_start,
       event_end,
       personnel,
+      event_image,
       description,
       archived,
       event_typeID
@@ -105,6 +108,7 @@ module.exports = models => {
         event_start: event_start,
         event_end: event_end,
         personnel: personnel,
+        event_image: event_image;
         description: description,
         archived: archived,
         event_typeID: event_typeID
