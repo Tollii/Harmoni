620a47d0864959808fbce8a86fe7e8ffaff82cba
andrtoln <andrtoln@stud.ntnu.no>
2020-01-16 14:24:22 +0100

    Created route for sending mails. Set up docs with swagger

--- backend/mailer/mailer.js
+++ backend/mailer/mailer.js
@@ -1,34 +1,33 @@
 const nodemailer = require("nodemailer");
 
-module.exports = () => {
 
-    return async () => {
 
-        const testAccount = await nodemailer.createTestAccount();
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false,
-            auth: {
-                user: 'gretchen.lubowitz23@ethereal.email',
-                pass: 'zvDtHYwEYPvjkhNfDF'
-            }
-        });
+const mailer =  async (recipients, subject, html) => {
 
-        const info = await transporter.sendMail({
-            from: '"Harmoni" <gretchen.lubowitz23@ethereal.email>',
-            to: "andreas.tolnes@ymail.com, andreas.tolnes@gmail.com",
-            subject: "Test mail from Harmoni",
-            html: "<b>YEET<b>"
-        });
+    const testAccount = await nodemailer.createTestAccount();
 
-        console.log("Message sent: %s", info.messageId);
+    const transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'gretchen.lubowitz23@ethereal.email',
+            pass: 'zvDtHYwEYPvjkhNfDF'
+        }
+    });
 
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl());
+    const info = await transporter.sendMail({
+        from: '"Harmoni" <gretchen.lubowitz23@ethereal.email>',
+        to: recipients,
+        subject: subject,
+        html: html
+    });
 
-    };
+    console.log("Message sent: %s", info.messageId);
+
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl());
+
 };
 
-
-
+module.exports = mailer;
---
+++ backend/router/mailer.js
@@ -0,0 +1,31 @@
+const mailer = require('../mailer/mailer');
+
+/**
+ * @typedef Mailer
+ * @property {string} recipients.required - Recipients email
+ * @property {string} subject.required - Subject of email
+ * @property {string} html.required - HTML of email
+ */
+
+
+module.exports = ( app, base ) => {
+
+    /**
+     * @group Mailer - Operations about mailer
+     * @route POST /mailer/
+     * @param {Mailer.model} mailer.body.required
+     * @returns {object} 200 - Email successfully sent
+     * @returns {Error}  default - Internal server error
+     */
+
+    app.post(base, (req, res) => {
+        try {
+            console.log(req.body.html);
+            mailer(req.body.recipients, req.body.subject, req.body.html);
+            res.status(200).send("Email successfully sent");
+        } catch(err) {
+            res.status(500).send("Internal server error");
+            console.log(err);
+        }
+    });
+};
