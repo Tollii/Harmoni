47eeca5e4be990246addf18dc2e39b23a806139d
Kasper Vedal Gundersen <kaspervg@stud.ntnu.no>
2020-01-20 11:10:08 +0100

    changePassword finished and working

--- frontend/src/service/Authentication.tsx
+++ frontend/src/service/Authentication.tsx
@@ -1,33 +1,38 @@
 import getCookie from "./cookie";
 import UserService from "./users";
-const axios = require("axios");
-let url: string = "localhost:";
-let port: number = 8080;
+const axios = require("axios").default;
+let url: string = "http://localhost:8080";
 
 const token = getCookie("token");
 
+export class Password {
+  old_password!: string;
+  new_password!: string;
+}
+
 class Authentication {
+  changePassword(password: any) {
+    return axios
+      .put(url + "/reset/", password, { headers: { token: token } })
+      .then((response: { data: Password }) => response.data);
+  }
   getLogin(credentials: any) {
     return axios
-      .put("http://" + url + port + "/login/", credentials)
+      .put(url + "/login/", credentials)
       .then((response: { data: Object }) => response.data);
-  };
+  }
   signUp(information: any) {
     return axios
-      .post("http://" + url + port + "/signup/", information)
+      .post(url + "/signup/", information)
       .then((response: { data: Object }) => response.data);
-  };
-  getAuth () {
-    if(token){
-      return UserService.getOneUser()
-      .then(user => user.roleID)
+  }
+  getAuth() {
+    if (token) {
+      return UserService.getOneUser().then(user => user.roleID);
     } else {
       return Promise.resolve(0);
     }
   }
 }
-
-
-
 
 export default new Authentication();
