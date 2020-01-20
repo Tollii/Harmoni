9f5355b85c0601949d56ff8cfa53d17a30ddeecb
Andreas Tolnes <andrtoln@stud.ntnu.no>
2020-01-20 11:24:32 +0100

    Backend/feat/mailer

--- backend/router/mailer.js
+++ backend/router/mailer.js
@@ -1,30 +1,46 @@
-const mailer = require('../mailer/mailer');
+module.exports = ( app, models, base, auth ) => {
 
-/**
- * @typedef Mailer
- * @property {string} recipients.required - Recipients email
- * @property {string} subject.required - Subject of email
- * @property {string} html.required - HTML of email
- */
-
-
-module.exports = ( app, base ) => {
+    const mailer = require('../mailer/mailer');
+    const eventController = require('../dao/events')(models);
+    const contractDao = require("../dao/contracts")(models);
 
     /**
      * @group Mailer - Operations about mailer
-     * @route POST /mailer/
-     * @param {Mailer.model} mailer.body.required
+     * @route POST /mailer/event/{id}
+     * @param {integer} id.path.required - event id
+     * @param {string} token.header.required - user token
      * @returns {object} 200 - Email successfully sent
      * @returns {Error}  default - Internal server error
      */
 
-    app.post(base, (req, res) => {
-        try {
-            mailer(req.body.recipients, req.body.subject, req.body.html);
-            res.status(200).send("Email successfully sent");
-        } catch(err) {
-            res.status(500).send("Internal server error");
-            console.log(err);
-        }
+    app.post(base + "/event/:id", (req, res) => {
+        auth.check_permissions(req.headers.token, ["Admin", "Organizer"]).then(value => {
+
+            if(value.auth) {
+                try {
+                    eventController.eventGetOne(req.params.id).then(resp => {
+                        contractDao.contractGetAllByEvent(req.params.id).then(data => {
+                            let recipients = '';
+                            data.map(e => {
+                                recipients += `${e.dataValues.username} <${e.dataValues.email}>, `;
+                            });
+                            const subject = `${resp.dataValues.event_name} has been cancelled.`;
+                            const html = `<h1>${resp.dataValues.event_name} has been cancelled</h1>` + '\n<p>Sorry for the inconvenience.</p>';
+                            mailer(recipients, subject, html);
+                        });
+                    });
+                    res.status(200).sendStatus("Mail sent")
+                } catch(err) {
+                    res.status(500).sendStatus("Internal server error");
+                    console.log(err);
+                }
+            }
+        });
     });
 };
+
+
+
+
+
+// res.sendStatus(200).send("Email successfully sent");
