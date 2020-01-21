328e225ee3038be6f874ff4f9beed7fda20e3b75
Nikolai Roede Dokken <nikolard@stud.ntnu.no>
2020-01-21 14:40:27 +0100

    Tickets can be updated and posted, added and deleted

--- backend/dao/events.js
+++ backend/dao/events.js
@@ -1,20 +1,25 @@
 module.exports = models => {
   const Event = models.Events;
+  const Ticket = models.Tickets;
   const Contract = models.Contracts;
-    var Sequelize = require('sequelize');
-    const Op = Sequelize.Op;
-    return {
+  var Sequelize = require("sequelize");
+  const Op = Sequelize.Op;
+  return {
     eventGetAll: async () => Event.findAll().then(events => events),
-        eventGetCarouselEvent:async ()=>Event.findAll({
-            where: {
-                archived: false
-            },limit:15,order:[['event_start','DESC']]
-        }).then(events => events),
+    eventGetCarouselEvent: async () =>
+      Event.findAll({
+        where: {
+          archived: false
+        },
+        limit: 15,
+        order: [["event_start", "DESC"]]
+      }).then(events => events),
 
-      eventGetAllUnarchived: async () => Event.findAll({
-          where: {
-              archived: false
-          }
+    eventGetAllUnarchived: async () =>
+      Event.findAll({
+        where: {
+          archived: false
+        }
       }).then(events => events),
 
     eventGetOne: async id =>
@@ -24,16 +29,18 @@ module.exports = models => {
         }
       }).then(events => events),
 
-        eventGetByUser: async (id)  =>
-            Event.findAll({
-                include: [{
-                    model: Contract,
-                    as: 'Contract',
-                    where: {
-                        userID: id
-                    }
-                }]
-            }).then(events => events),
+    eventGetByUser: async id =>
+      Event.findAll({
+        include: [
+          {
+            model: Contract,
+            as: "Contract",
+            where: {
+              userID: id
+            }
+          }
+        ]
+      }).then(events => events),
 
     eventDelete: async id =>
       Event.findOne({
@@ -88,24 +95,24 @@ module.exports = models => {
       });
     },
 
-      eventArchive: async (
-          current_time,
-      ) => {
-          return Event.update(
-                  {
-                      archived: true
-                  },
-                  {
-                      where: {
-                          event_end: {
-                              // if current_time < event_end
-                              [Op.lt]: current_time
-                          }
-                      }
-                  }
-              ).then(events => events);
-          },
+    eventArchive: async current_time => {
+      return Event.update(
+        {
+          archived: true
+        },
+        {
+          where: {
+            event_end: {
+              // if current_time < event_end
+              [Op.lt]: current_time
+            }
+          }
+        }
+      ).then(events => events);
+    },
 
+    ticketDeleteByEvent: async event_ID =>
+      Ticket.destroy({ where: { eventID: event_ID } }).then(events => events),
 
     eventCreate: async (
       event_name,
