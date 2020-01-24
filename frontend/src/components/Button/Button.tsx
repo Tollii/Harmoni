44acc388be18621d2f320740a3b54944dbc0a8ff
Kristoffer Vanebo <krisvane@stud.ntnu.no>
2020-01-24 16:17:42 +0100

    fix + added comment for button

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -6,6 +6,12 @@ interface Button {
     children:any;
 }
 
+/**
+ * Creates a button
+ * @param fontWeight sets the fontweight of the button
+ * @param childre sets the children of the button
+ * @returns returns a button
+ */
 export default function Button(props:any, {fontWeight, children}: Button){
     return (
         <MatButton {...props} color="secondary" style={{fontWeight: fontWeight}}>{children}</MatButton>
